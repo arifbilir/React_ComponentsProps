ea40ea6d2ede944ededb4bd8043672d6d1e95036
arifbilir <arifbilirde@gmail.com>
2023-03-02 18:04:32 +0100

    main

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import Nav from "../nav/Nav"
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerRight">
+            <h3>
+                <span className="arif">Arif Bilir </span>
+                Frontend Developer
+            </h3>
+        </div>
+        <Nav/>
+
+    </div>
+  )
+}
+
+export default Header
